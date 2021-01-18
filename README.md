9484af22f0ed3292009d7da379652f135f9e9fe2
Dr Maxim Orlovsky <orlovsky@pandoracore.com>
2021-01-18 23:00:20 +0100

    CI & README

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # Descriptor wallet library
 
-Library for building descriptor-based bitcoin wallets
+![Build](https://github.com/LNP-BP/descriptor-wallet/workflows/Build/badge.svg)
+![Tests](https://github.com/LNP-BP/descriptor-wallet/workflows/Tests/badge.svg)
+![Lints](https://github.com/LNP-BP/descriptor-wallet/workflows/Lints/badge.svg)
+[![codecov](https://codecov.io/gh/LNP-BP/descriptor-wallet/branch/master/graph/badge.svg)](https://codecov.io/gh/LNP-BP/descriptor-wallet)
+
+[![crates.io](https://meritbadge.herokuapp.com/descriptor-wallet)](https://crates.io/crates/descriptor-wallet)
+[![Docs](https://docs.rs/descriptor-wallet/badge.svg)](https://docs.rs/descriptor-wallet)
+[![unsafe forbidden](https://img.shields.io/badge/unsafe-forbidden-success.svg)](https://github.com/rust-secure-code/safety-dance/)
+[![MIT licensed](https://img.shields.io/github/license/LNP-BP/descriptor-wallet)](./LICENSE)
+
+Library for building descriptor-based bitcoin wallets. Previously was a part of
+[LNP/BP Core Library](https://github.com/LNP-BP/rust-lnpbp).
+
+Components:
+- More efficient manipulations with BIP-32 derivation paths
+- Universal miniscript/classical bitcoin descriptors
+- Script templates allowing embedding extended pubkeys into bitcoin script 
+  assembly
+- Lexicographic ordering of transaction & PSBT inputs & oututs
+- Script type system
+- Helper types for working with hash-lock contracts
+- PSBT utility functions (retrieving previous output, computing fee)
+
+Includes separate crate for SLIP-132 functionality in [`/slip132`](./slip132)
+subdirectory.
---
+++ contrib/test.sh
@@ -0,0 +1,76 @@
+#!/bin/sh -ex
+
+AS_DEPENDENCY=true
+DO_LINT=true
+
+FEATURES="serde"
+
+if [ "$DO_COV" = true ]
+then
+    export RUSTFLAGS="-C link-dead-code"
+fi
+
+
+# Use toolchain if explicitly specified
+if [ -n "$TOOLCHAIN" ]
+then
+    alias cargo="cargo +$TOOLCHAIN"
+fi
+
+# Check that we can build w/o features
+cargo check --verbose --all-targets --workspace
+cargo check --verbose --no-default-features --all-targets --workspace
+
+# Check that we can build with each feature
+for feature in ${FEATURES}
+do
+    cargo check --verbose --features="$feature" --all-targets
+done
+
+# Check that we can build services with different features
+cargo check --manifest-path slip132/Cargo.toml --verbose --all-targets
+cargo check --manifest-path slip132/Cargo.toml --verbose --no-default-features --all-targets
+for feature in "serde"
+do
+    cargo check --manifest-path slip132/Cargo.toml --verbose --features="$feature" --all-targets
+done
+
+# Fuzz if told to
+if [ "$DO_FUZZ" = true ]
+then
+    (
+        cd fuzz
+        cargo test --verbose --all-targets
+        ./travis-fuzz.sh
+    )
+fi
+
+# Bench if told to
+if [ "$DO_BENCH" = true ]
+then
+    cargo bench --features unstable --all-targets
+fi
+
+# Use as dependency if told to
+if [ -n "$AS_DEPENDENCY" ]
+then
+    rm -rf dep_test
+    cargo new dep_test
+    cd dep_test
+    cat ../contrib/depCargo.toml >> Cargo.toml
+    cargo build --verbose
+    cd ..
+    rm -rf dep_test
+fi
+
+# Test all features
+cargo test --verbose --all-features --all-targets --workspace
+
+# Lint if told to
+if [ "$DO_LINT" = true ]
+then
+    (
+        rustup component add rustfmt
+        cargo fmt --all -- --check
+    )
+fi
