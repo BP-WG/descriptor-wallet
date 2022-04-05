350e978d6295db657e24bd3df5ac9e88b239b055
Dr Maxim Orlovsky <orlovsky@pandoracore.com>
2022-04-05 19:54:44 +0200

    PSBT serialization unit tests

--- psbt/src/modern/psbt.rs
+++ psbt/src/modern/psbt.rs
@@ -217,3 +217,69 @@ impl FromStr for Psbt {
         )
     }
 }
+
+#[cfg(test)]
+mod test {
+    use strict_encoding::StrictDecode;
+    use strict_encoding_test::test_encoding_roundtrip;
+
+    use super::*;
+
+    #[test]
+    fn psbt_bip174_serialization() {
+        let hex = "\
+            70736274ff0100750200000001268171371edff285e937adeea4b37b78000c0566\
+            cbb3ad64641713ca42171bf60000000000feffffff02d3dff505000000001976a91\
+            4d0c59903c5bac2868760e90fd521a4665aa7652088ac00e1f5050000000017a914\
+            3545e6e33b832c47050f24d3eeb93c9c03948bc787b32e1300000100fda50101000\
+            00000010289a3c71eab4d20e0371bbba4cc698fa295c9463afa2e397f8533ccb62f\
+            9567e50100000017160014be18d152a9b012039daf3da7de4f53349eecb985fffff\
+            fff86f8aa43a71dff1448893a530a7237ef6b4608bbb2dd2d0171e63aec6a4890b4\
+            0100000017160014fe3e9ef1a745e974d902c4355943abcb34bd5353ffffffff020\
+            0c2eb0b000000001976a91485cff1097fd9e008bb34af709c62197b38978a4888ac\
+            72fef84e2c00000017a914339725ba21efd62ac753a9bcd067d6c7a6a39d0587024\
+            7304402202712be22e0270f394f568311dc7ca9a68970b8025fdd3b240229f07f8a\
+            5f3a240220018b38d7dcd314e734c9276bd6fb40f673325bc4baa144c800d2f2f02\
+            db2765c012103d2e15674941bad4a996372cb87e1856d3652606d98562fe39c5e9e\
+            7e413f210502483045022100d12b852d85dcd961d2f5f4ab660654df6eedcc794c0\
+            c33ce5cc309ffb5fce58d022067338a8e0e1725c197fb1a88af59f51e44e4255b20\
+            167c8684031c05d1f2592a01210223b72beef0965d10be0778efecd61fcac6f79a4\
+            ea169393380734464f84f2ab300000000000000";
+
+        let psbt = Psbt::from_str(hex).unwrap();
+        let hex_prime = psbt.to_string();
+        let psbt_prime = Psbt::deserialize(&Vec::from_hex(&hex_prime).unwrap()).unwrap();
+        assert_eq!(psbt, psbt_prime);
+        assert_eq!(hex, hex_prime);
+    }
+
+    #[test]
+    fn psbt_strict_encoding() {
+        let psbt_bytes = Vec::from_hex(
+            "00000002000000b32e130001000000268171371edff285e937adeea4b37b78000c\
+            0566cbb3ad64641713ca42171bf60000000001feffffff000001010000000001028\
+            9a3c71eab4d20e0371bbba4cc698fa295c9463afa2e397f8533ccb62f9567e50100\
+            000017160014be18d152a9b012039daf3da7de4f53349eecb985ffffffff86f8aa4\
+            3a71dff1448893a530a7237ef6b4608bbb2dd2d0171e63aec6a4890b40100000017\
+            160014fe3e9ef1a745e974d902c4355943abcb34bd5353ffffffff0200c2eb0b000\
+            000001976a91485cff1097fd9e008bb34af709c62197b38978a4888ac72fef84e2c\
+            00000017a914339725ba21efd62ac753a9bcd067d6c7a6a39d05870247304402202\
+            712be22e0270f394f568311dc7ca9a68970b8025fdd3b240229f07f8a5f3a240220\
+            018b38d7dcd314e734c9276bd6fb40f673325bc4baa144c800d2f2f02db2765c012\
+            103d2e15674941bad4a996372cb87e1856d3652606d98562fe39c5e9e7e413f2105\
+            02483045022100d12b852d85dcd961d2f5f4ab660654df6eedcc794c0c33ce5cc30\
+            9ffb5fce58d022067338a8e0e1725c197fb1a88af59f51e44e4255b20167c868403\
+            1c05d1f2592a01210223b72beef0965d10be0778efecd61fcac6f79a4ea16939338\
+            0734464f84f2ab30000000000000000000000000000000000000000000000000000\
+            00000000000000000002000000d3dff50500000000190076a914d0c59903c5bac28\
+            68760e90fd521a4665aa7652088ac000000000000000000000000010000e1f50500\
+            0000001700a9143545e6e33b832c47050f24d3eeb93c9c03948bc78700000000000\
+            000000000000000000000",
+        )
+        .unwrap();
+
+        let psbt = Psbt::strict_deserialize(&psbt_bytes).unwrap();
+
+        test_encoding_roundtrip(&psbt, &psbt_bytes).unwrap();
+    }
+}
