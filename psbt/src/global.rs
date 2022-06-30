98fe68c54d27f167000db8d1d7b5cdf00267a969
Dr Maxim Orlovsky <orlovsky@pandoraprime.ch>
2022-06-30 16:42:20 +0200

    Bump strict encoding to v0.8

--- psbt/src/global.rs
+++ psbt/src/global.rs
@@ -314,9 +314,6 @@ impl FromStr for Psbt {
 
 #[cfg(test)]
 mod test {
-    use strict_encoding::StrictDecode;
-    use strict_encoding_test::test_encoding_roundtrip;
-
     use super::*;
 
     #[test]
@@ -345,35 +342,5 @@ mod test {
         let psbt_prime = Psbt::deserialize(&Vec::from_hex(&hex_prime).unwrap()).unwrap();
         assert_eq!(psbt, psbt_prime);
         assert_eq!(hex, hex_prime);
-    }
-
-    #[test]
-    fn psbt_strict_encoding() {
-        let psbt_bytes = Vec::from_hex(
-            "000000000200000001b32e130001000000268171371edff285e937adeea4b37b78\
-            000c0566cbb3ad64641713ca42171bf60000000001feffffff00000101000000000\
-            10289a3c71eab4d20e0371bbba4cc698fa295c9463afa2e397f8533ccb62f9567e5\
-            0100000017160014be18d152a9b012039daf3da7de4f53349eecb985ffffffff86f\
-            8aa43a71dff1448893a530a7237ef6b4608bbb2dd2d0171e63aec6a4890b4010000\
-            0017160014fe3e9ef1a745e974d902c4355943abcb34bd5353ffffffff0200c2eb0\
-            b000000001976a91485cff1097fd9e008bb34af709c62197b38978a4888ac72fef8\
-            4e2c00000017a914339725ba21efd62ac753a9bcd067d6c7a6a39d0587024730440\
-            2202712be22e0270f394f568311dc7ca9a68970b8025fdd3b240229f07f8a5f3a24\
-            0220018b38d7dcd314e734c9276bd6fb40f673325bc4baa144c800d2f2f02db2765\
-            c012103d2e15674941bad4a996372cb87e1856d3652606d98562fe39c5e9e7e413f\
-            210502483045022100d12b852d85dcd961d2f5f4ab660654df6eedcc794c0c33ce5\
-            cc309ffb5fce58d022067338a8e0e1725c197fb1a88af59f51e44e4255b20167c86\
-            84031c05d1f2592a01210223b72beef0965d10be0778efecd61fcac6f79a4ea1693\
-            93380734464f84f2ab3000000000000000000000000000000000000000000000000\
-            000000000000000000000002000000d3dff50500000000190076a914d0c59903c5b\
-            ac2868760e90fd521a4665aa7652088ac000000000000000000000000010000e1f5\
-            05000000001700a9143545e6e33b832c47050f24d3eeb93c9c03948bc7870000000\
-            00000000000000000000000000000",
-        )
-        .unwrap();
-
-        let psbt = Psbt::strict_deserialize(&psbt_bytes).unwrap();
-
-        test_encoding_roundtrip(&psbt, &psbt_bytes).unwrap();
     }
 }
