136e8a3af36a99b9e1107b9044a6bf09307cfd4d
Dr. Maxim Orlovsky <orlovsky@pandoraprime.ch>
2022-11-17 12:31:09 +0100

    Fix bitcoin_onchain feature use. Remove miniscript functionality

--- onchain/src/resolvers/mod.rs
+++ onchain/src/resolvers/mod.rs
@@ -1,12 +1,9 @@
-// Descriptor wallet library extending bitcoin & miniscript functionality
-// by LNP/BP Association (https://lnp-bp.org)
+// Wallet-level libraries for bitcoin protocol by LNP/BP Association
+//
 // Written in 2020-2022 by
 //     Dr. Maxim Orlovsky <orlovsky@lnp-bp.org>
 //
-// To the extent possible under law, the author(s) have dedicated all
-// copyright and related and neighboring rights to this software to
-// the public domain worldwide. This software is distributed without
-// any warranty.
+// This software is distributed without any warranty.
 //
 // You should have received a copy of the Apache-2.0 License
 // along with this software.
@@ -74,68 +71,6 @@ pub trait ResolveUtxo {
         &self,
         scripts: impl IntoIterator<Item = &'script Script> + Clone,
     ) -> Result<Vec<HashSet<Utxo>>, UtxoResolverError>;
-
-    /// Finds UTXO set for the addresses derivable from the given descriptor
-    #[cfg(all(feature = "miniscript", feature = "__ignore"))]
-    fn resolve_descriptor_utxo<C: Verification>(
-        &self,
-        secp: &Secp256k1<C>,
-        descriptor: &Descriptor<DerivationAccount>,
-        terminal_derivation: impl AsRef<[UnhardenedIndex]>,
-        from_index: UnhardenedIndex,
-        count: u32,
-    ) -> Result<BTreeMap<UnhardenedIndex, (Script, HashSet<Utxo>)>, UtxoResolverError> {
-        use std::cell::RefCell;
-        use std::rc::Rc;
-
-        use bitcoin::secp256k1::{Secp256k1, Verification};
-        use bitcoin_hd::{
-            DerivationAccount, Descriptor as BpDescriptor, SegmentIndexes, UnhardenedIndex,
-        };
-        use miniscript::Descriptor;
-
-        let terminal_derivation = terminal_derivation.as_ref();
-        let mut derivation = Vec::<UnhardenedIndex>::with_capacity(terminal_derivation.len() + 1);
-        derivation.extend(terminal_derivation);
-        derivation.push(UnhardenedIndex::zero());
-        let derivation = Rc::new(RefCell::new(derivation));
-
-        let indexes = (0..count)
-            .into_iter()
-            .map(|offset| {
-                from_index.checked_add(offset).ok_or_else(|| {
-                    UtxoResolverError::IndexOutOfRange(
-                        from_index.first_index() as usize + offset as usize,
-                    )
-                })
-            })
-            .collect::<Result<Vec<_>, UtxoResolverError>>()?;
-
-        let scripts = indexes
-            .into_iter()
-            .map(|index| {
-                if let Some(i) = derivation.borrow_mut().last_mut() {
-                    *i = index
-                }
-                Ok((
-                    index,
-                    BpDescriptor::<bitcoin::PublicKey>::script_pubkey(
-                        descriptor,
-                        secp,
-                        &*derivation.borrow(),
-                    )?,
-                ))
-            })
-            .collect::<Result<BTreeMap<_, _>, DeriveError>>()?;
-
-        Ok(self
-            .resolve_utxo(scripts.values())?
-            .into_iter()
-            .zip(scripts.keys())
-            .zip(scripts.values())
-            .map(|((utxo_set, index), script)| (*index, (script.clone(), utxo_set)))
-            .collect())
-    }
 }
 
 impl ResolveTx for BTreeMap<Txid, Transaction> {
