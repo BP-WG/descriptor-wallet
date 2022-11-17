d0f96ceaf87f676ff1e51eed18032ed784cdd24c
Dr Maxim Orlovsky <orlovsky@pandoraprime.ch>
2022-11-17 13:21:57 +0100

    Remove excessive serde feature hierarchy

--- slip132/src/lib.rs
+++ slip132/src/lib.rs
@@ -1,13 +1,11 @@
-// SLIP-132 library for parsing custom xpub/xpriv key formats
-// Written in 2021 by
+// Wallet-level libraries for bitcoin protocol by LNP/BP Association
+//
+// Written in 2020-2022 by
 //     Dr. Maxim Orlovsky <orlovsky@lnp-bp.org>
 //
-// To the extent possible under law, the author(s) have dedicated all
-// copyright and related and neighboring rights to this software to
-// the public domain worldwide. This software is distributed without
-// any warranty.
+// This software is distributed without any warranty.
 //
-// You should have received a copy of the Apache 2.0 License
+// You should have received a copy of the Apache-2.0 License
 // along with this software.
 // If not, see <https://opensource.org/licenses/Apache-2.0>.
 
