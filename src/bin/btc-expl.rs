7e38d327cbff2df32205ff71e889a6d400700686
Dr Maxim Orlovsky <orlovsky@lnp-bp.org>
2024-03-17 11:51:46 +0100

    chore: fix fmt

--- src/bin/btc-expl.rs
+++ src/bin/btc-expl.rs
@@ -237,19 +237,33 @@ impl Args {
                 Some(WitnessVersion::V0) if prevout.script_pubkey.is_v0_p2wpkh() => {
                     let mut iter = txin.witness.iter();
                     let Some(sersig) = iter.next() else {
-                        eprintln!("  {}", "invalid witness structure for P2WPK output".bright_red());
+                        eprintln!(
+                            "  {}",
+                            "invalid witness structure for P2WPK output".bright_red()
+                        );
                         continue;
                     };
                     let Ok(sig) = EcdsaSig::from_slice(sersig) else {
-                        eprintln!("    {} {}", "invalid signature".bright_red(), sersig.to_hex());
+                        eprintln!(
+                            "    {} {}",
+                            "invalid signature".bright_red(),
+                            sersig.to_hex()
+                        );
                         continue;
                     };
                     let Some(serpk) = iter.next() else {
-                        eprintln!("  {}", "invalid witness structure for P2WPK output".bright_red());
+                        eprintln!(
+                            "  {}",
+                            "invalid witness structure for P2WPK output".bright_red()
+                        );
                         continue;
                     };
                     let Ok(pk) = PublicKey::from_slice(serpk) else {
-                        eprintln!("    {} {}", "invalid public key".bright_red(), serpk.to_hex());
+                        eprintln!(
+                            "    {} {}",
+                            "invalid public key".bright_red(),
+                            serpk.to_hex()
+                        );
                         continue;
                     };
                     println!("  wpkh({pk})");
